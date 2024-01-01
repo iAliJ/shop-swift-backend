26ba953d5ee122002393f604cc021322e13073ff
alij <alimjahrami@gmail.com>
2024-01-01 18:24:48 +0300

    handle images as req file and body

--- helper/cloudUploader.js
+++ helper/cloudUploader.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 const cloudinary = require('cloudinary');
 const fileServices = require('../services/fileServices');
 
-exports.uploadFile = (field) => {
-    return (req, res, next) => {
-        cloudinary.uploader.upload(req.file.path)
-        .then(result => {
-            req.body[field] = result.url;
-            fileServices.removeFile(req.file.path);
-            next();
-        })
-    }
+// exports.uploadFile = (field) => {
+//     return (req, res, next) => {
+//         cloudinary.uploader.upload(req.file.path)
+//         .then(result => {
+//             req.body[field] = result.url;
+//             fileServices.removeFile(req.file.path);
+//             next();
+//         })
+//     }
+// }
+
+exports.uploadSingle = async (filePath) => {
+    const result = await cloudinary.uploader.upload(filePath);
+    fileServices.removeFile(filePath);
+    return result;
 }
