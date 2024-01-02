5abbc28c6fab58d20302dfa5d0387dcd3afcc862
faisal86 <faisalkarim203@gmail.com>
2024-01-02 09:56:33 +0300

    productmodel

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const categorySchema = mongoose.Schema({
     "name": {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     "user": {
         type: mongoose.Schema.Types.ObjectId,
