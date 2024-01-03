6ef20b2da96881669575cb06dd08d8e8a41c8415
alij <alimjahrami@gmail.com>
2024-01-03 09:37:26 +0300

    add api endpoint - delete cart item

--- controllers/cart.js
+++ controllers/cart.js
@@ -6,8 +6,11 @@ exports.cart_detail_get = async (req, res) => {
     // get the cart id owned by the user
     let cart = await Cart.findOne({user: req.user.id});
     // get all cart items owned by the cart
-    let cartItems = await CartItem.find({cart: cart._id});
-    res.json({cartItems});
+    CartItem.find({cart: cart._id})
+    .populate('product')
+    .then((cartItems) => {
+        res.json({cartItems});
+    })
 }
 
 function calculatePrice(singleItem, quantity) {
--- controllers/cartItem.js
+++ controllers/cartItem.js
@@ -19,10 +19,40 @@ exports.cartItem_update_post = async (req, res) => {
         new: true,
         upsert: true
     })
+    .populate('product')
     .then((newCartItem) => {
         console.log(`new cart item created with id: ${newCartItem._id}`);
         res.json({newCartItem});
     })
+}
+
+exports.cartItem_delete_get = async (req, res) => {
+    // get the cart item from url
+    const cartItemId = req.query.id;
+    console.log(`user ${req.user.id} attempting to delete cart item ${cartItemId}`);
+    try{
+        let cartItem = await CartItem.findById(cartItemId).populate('cart');
+        // check if current user has permission to delete the cart item
+        if(cartItem.cart.user == req.user.id) {
+            // delete cart item
+            CartItem.findByIdAndDelete(cartItemId)
+            .then((item) => {
+                console.log(`cart item ${cartItemId} deleted by ${req.user.id}`);
+                res.json({item});
+            })
+            .catch((err) => {
+                console.log('Error 01 deleting cart item');
+                res.json({"message": err});
+            })
+        }else{
+            console.log(`User ${req.user.id} attempted to delete an unauthorized cart item ${cartItemId}`);
+        }
+    }
+    catch(err){
+        console.log('Error 02 deleting cart item');
+        console.log(err);
+        res.json({"message": err});
+    }
 }
 
 function calculatePrice(singleItem, quantity) {
